7c0348cd6a06096c5aff51041ab641bd057fe88f
Kevin Campos <kesc23@hotmail.com>
2024-09-23 17:23:27 -0300

    add parse helper methods
    add methods to help parse dict fields when they're list with float32 values:
    
     - parse_extra_list
     - parse_dict_fields
     - parse_embeddings

--- response.py
+++ response.py
@@ -6,6 +6,22 @@ def success_response( data ):
 def error_response( error_code: int, message: str ):
     return { "code": error_code, "message": message }
 
+def parse_extra_list( item ):
+    if type(item) == dict:
+        item = parse_dict_fields(item)
+
+    return item
+
+def parse_dict_fields( _dict: dict ):
+    for key, value in _dict.items():
+        if isinstance( value, list ) and isinstance(value[0], np.float32):
+            _dict[key] = parse_embeddings( value )
+    return _dict
+
+def parse_embeddings( vec_array: list ):
+    vec_array = list(map( lambda x: float(x) if isinstance(x, np.float32) else x, vec_array))
+    return vec_array
+
 def execute( x ):
     try:
         data = x()
