fa04c22f50e5aedd56a252559c5f86bc83897f9f
Kevin Campos <kesc23@hotmail.com>
2024-09-23 10:54:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ from env import environment
 from server import server as app
 from routes.collection import *
 
-app.run(port=19530, debug=(environment["DEBUG"] == "true"), host="0.0.0.0")
+
+if __name__ == "__main__":
+    try:
+        app.start_milvus()
+        app.run(port=19530, host="0.0.0.0")
+    finally:
+        app.close_milvus()
