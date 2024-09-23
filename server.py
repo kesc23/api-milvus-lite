ce76a1168ef8939d0556f1863485685af1b21ff7
Kevin Campos <kesc23@hotmail.com>
2024-09-23 10:52:44 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 from env import environment
-from flask import Flask
-from pymilvus import MilvusClient
+from flask_milvus import Server
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-client = MilvusClient("./data/milvus.db")
-
-server = Flask(__name__)
+server = Server(__name__)
 
 if environment["ENV_PRODUCTION"] == "true":
     server.wsgi_app = ProxyFix( server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1 )
