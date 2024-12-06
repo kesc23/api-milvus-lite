e1305b0097e8af50829479dd20fbbbdf58db384b
Kevin Campos <kesc23@hotmail.com>
2024-12-06 10:32:41 -0300

    Update server.py

--- server.py
+++ server.py
@@ -4,5 +4,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 server = Server(__name__)
 
-if environment["ENV_PRODUCTION"] == "true":
-    server.wsgi_app = ProxyFix( server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1 )
+# if environment["ENV_PRODUCTION"] == "true":
+
+    # server.wsgi_app = ProxyFix( server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1 )
