3ee2318d69790540d2003f2d3864cb96e04bce7e
Kevin Campos <kesc23@hotmail.com>
2024-12-06 10:32:03 -0300

    add hypercorn to start

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ from server import server as app
 from routes.collection import *
 from routes.vectors import *
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":   
+    import hypercorn.asyncio
+    import asyncio
+    from hypercorn.config import Config
+
+    config = Config()
+    config.bind = ["0.0.0.0:19530"]
+    config.alpn_protocols = ["h2"]
+
     try:
         app.start_milvus()
-        app.run(port=19530, host="0.0.0.0")
+        asyncio.run(hypercorn.asyncio.serve(app, config))
     finally:
         app.close_milvus()
